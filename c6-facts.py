8edaffcd531981fd1e2a606d230f0c376e90b84c
Yasser Saied <ysaied80@yahoo.com>
2020-02-20 10:04:19 +0400

    Update c6-facts.py

--- c6-facts.py
+++ c6-facts.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 from jnpr.junos import Device
+form pprint import pprint
+
 
 dev_mgmt = { "KIF_VPN" : "10.117.97.56", 
    "HRZ_VPN" : "10.117.97.55", 
@@ -15,6 +17,6 @@ for src_node, mgmt_ip in dev_mgmt.items():
    dev.open()
    print ( "="*20 + src_node + "="*20)
    dev_info = dev.facts
-   print (dev_info)
+   pprint (dev_info)
    print ("="*20 + "="*len(src_node)  + "="*20)
    dev.close()
