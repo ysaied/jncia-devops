403e3cd9c1e1010fa382ec4d637abd47b29db3b6
Yasser Saied <ysaied80@yahoo.com>
2020-02-20 10:02:27 +0400

    test

---
+++ c6-facts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from jnpr.junos import Device
+
+dev_mgmt = { "KIF_VPN" : "10.117.97.56", 
+   "HRZ_VPN" : "10.117.97.55", 
+   "AMS_VPN" : "10.117.97.37", 
+   "LON_VPN" : "10.117.97.36", 
+   "Partner" : "10.117.97.39"
+   } 
+
+login_username = "ysaied"
+
+for src_node, mgmt_ip in dev_mgmt.items():
+   dev = Device(host= mgmt_ip, user= login_username)
+   dev.open()
+   print ( "="*20 + src_node + "="*20)
+   dev_info = dev.facts
+   print ("="*20 + "="*len(src_node)  + "="*20)
+   dev.close()
---
+++ device-info.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+from jnpr.junos import Device
+from lxml import etree
+import json
+from jnpr.junos.utils.fs import FS
+from datetime import datetime
+from pprint import pprint
+
+dev_mgmt = { "KIF_VPN" : "10.117.97.56", 
+   "HRZ_VPN" : "10.117.97.55", 
+   "AMS_VPN" : "10.117.97.37", 
+   "LON_VPN" : "10.117.97.36", 
+   "Partner" : "10.117.97.39"
+   } 
+
+login_username = "ysaied"
+
+sh_conf_sys_login_xml = """
+<get-config>
+  <source><running/></source>
+  <filter type="subtree">
+    <configuration>
+      <system>
+        <login/>
+      </system>
+    </configuration>
+  </filter>
+</get-config>
+"""
+
+
+for src_node, mgmt_ip in dev_mgmt.items():
+   dev = Device(host= mgmt_ip, user= login_username)
+   dev.open()
+   dev_info = dev.facts
+#   pprint (dev_info)
+   print ( "version {} model {}".format(dev.facts["version_RE1"],dev.facts["model"]))
+#   for key, value in dev_info.items():
+#      print ("{} ----> {}".format(key, value))
+   print ("="*50)
+   dev.close()
